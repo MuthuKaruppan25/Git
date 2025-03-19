66c52b9405d5cbbb913422d6c32c100e71ef1441
MuthuKaruppan25 <muthukaruppanaakash76@gmail.com>
2025-03-19 12:33:44 +0530

    Checking for existing es-lint errors

--- Task-8/error.js
+++ Task-8/error.js
@@ -1,9 +1,2 @@
-var unusedVariable = "Hello";  // ❌ 'unusedVariable' is assigned a value but never used (no-unused-vars)
-
-function testFunction() { 
-console.log("This is a test")   // ❌ Missing semicolon (semi)
-}  
-
-let x= 5  // ❌ Unexpected spaces around "=" (space-infix-ops)
-
-if(true){console.log("No space after if") } // ❌ Missing space after `if` (keyword-spacing)
+var x=5;
+console.log(x);
